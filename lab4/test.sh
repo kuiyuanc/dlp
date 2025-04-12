fb836282528330c8b2092a0f0210d2687712bc72
110550035 <kono.c@nycu.edu.tw>
2025-04-12 14:41:39 +0800

    chore: update training/testing scripts

--- lab4/test.sh
+++ lab4/test.sh
@@ -4,5 +4,14 @@
 #SBATCH -N 1                    # Maximum number of nodes to be allocated
 #SBATCH --gres=gpu:1            # specify GPU number
 # 還有很多其他參數，可參考官方文件(https://slurm.schedmd.com/sbatch.html)
+
+COMMON_ARGS = --DR ../LAB4_Dataset --num_workers 1
+NO_TFR_ARGS = --tfr 0 --tfr_d_step 0
+
 export LD_PRELOAD=/lib/x86_64-linux-gnu/libcuda.so.1
-cd Lab4_template; srun python3 Tester.py --DR ../LAB4_Dataset --save_root ../data --ckpt_path ../checkpoints/epoch=70.ckpt --num_workers 1
+
+cd Lab4_template
+srun python3 Tester.py $(COMMON_ARGS) --save_root ../data/tfr=0.0/kl=None --ckpt_path ../checkpoints/tfr=0.0/kl=None/epoch=70.ckpt $(NO_TFR_ARGS) --kl_anneal_type Without
+srun python3 Tester.py $(COMMON_ARGS) --save_root ../data/tfr=0.0/kl=Monotonic --ckpt_path ../checkpoints/tfr=0.0/kl=Monotonic/epoch=70.ckpt $(NO_TFR_ARGS) --kl_anneal_type Monotonic
+srun python3 Tester.py $(COMMON_ARGS) --save_root ../data/tfr=0.0/kl=Cyclical --ckpt_path ../checkpoints/tfr=0.0/kl=Cyclical/epoch=70.ckpt $(NO_TFR_ARGS)
+srun python3 Tester.py $(COMMON_ARGS) --save_root ../data/tfr=1.0/kl=None --ckpt_path ../checkpoints/tfr=1.0/kl=None/epoch=70.ckpt --kl_anneal_type Without
--- lab4/train.sh
+++ lab4/train.sh
@@ -4,5 +4,14 @@
 #SBATCH -N 1                    # Maximum number of nodes to be allocated
 #SBATCH --gres=gpu:1            # specify GPU number
 # 還有很多其他參數，可參考官方文件(https://slurm.schedmd.com/sbatch.html)
+
+COMMON_ARGS = --DR ../LAB4_Dataset --fast_train --store_visualization --num_workers 1 --per_save 10
+NO_TFR_ARGS = --tfr 0 --tfr_d_step 0
+
 export LD_PRELOAD=/lib/x86_64-linux-gnu/libcuda.so.1
-cd Lab4_template; srun python3 Trainer.py --DR ../LAB4_Dataset --save_root ../checkpoints --fast_train --store_visualization --num_workers 1 --per_save 10
+
+cd Lab4_template
+srun python3 Trainer.py $(COMMON_ARGS) --save_root ../checkpoints/tfr=0.0/kl=None $(NO_TFR_ARGS) --kl_anneal_type Without
+srun python3 Trainer.py $(COMMON_ARGS) --save_root ../checkpoints/tfr=0.0/kl=Monotonic $(NO_TFR_ARGS) --kl_anneal_type Monotonic
+srun python3 Trainer.py $(COMMON_ARGS) --save_root ../checkpoints/tfr=0.0/kl=Cyclical $(NO_TFR_ARGS)
+srun python3 Trainer.py $(COMMON_ARGS) --save_root ../checkpoints/tfr=1.0/kl=None --kl_anneal_type Without
