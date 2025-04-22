93cc0943a9c349319d633076f81d4f4373d44dea
110550035 <kono.c@nycu.edu.tw>
2025-04-23 00:41:59 +0800

    chore: update automation script for nycu csit operation

---
+++ lab5/test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH -A dlp_acct             # Account name (可透過 sshare 查看)
+#SBATCH -p dlp_nodes            # Partition name (可透過 sinfo 查看 partition)
+#SBATCH -N 1                    # Maximum number of nodes to be allocated
+#SBATCH --gres=gpu:1            # specify GPU number
+# 還有很多其他參數，可參考官方文件(https://slurm.schedmd.com/sbatch.html)
+
+export LD_PRELOAD=/lib/x86_64-linux-gnu/libcuda.so.1
+
+srun python3 src/test_model.py "$*"
