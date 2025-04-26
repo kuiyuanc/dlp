edc67d59e0f06d686ce4dcab1cecdd480f6dde1e
110550035 <kono.c@nycu.edu.tw>
2025-04-27 01:31:24 +0800

    chore: remove process & thread # constraints

--- lab5/train.sh
+++ lab5/train.sh
@@ -7,4 +7,4 @@
 
 export LD_PRELOAD=/lib/x86_64-linux-gnu/libcuda.so.1
 
-srun --ntasks=1 --cpus-per-task=8 --mem=8G python3 src/train.py $* --device cuda
+srun --mem=8G python3 src/train.py $* --device cuda
